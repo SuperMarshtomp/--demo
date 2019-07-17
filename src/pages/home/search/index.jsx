b57f009943a63dd3474ec7caa470554d6a48109c
ChenMou <1106417223@qq.com>
2019-07-17 09:36:44 +0800

    实现图标点击切换

--- src/pages/home/search/index.jsx
+++ src/pages/home/search/index.jsx
@@ -6,22 +6,29 @@ import icon_1 from './assets/icon_1.jpg'
 import icon_2 from './assets/icon_2.jpg'
 
 export default class Search extends Component {
+    state = {
+        isList: true
+    }
+
+    handleClick = () => {
+        console.log('handleIconClick');
+        this.setState({
+            isList: !this.state.isList
+        })
+    }
+
     render () {
         return (
             <View className='search'>
-                <View className='img-view'>
-                    <Image className='img' src={ icon_2 }/>
+                <View className='img-view' onClick={this.handleClick}>
+                    <Image className='img' src={this.state.isList ? icon_2 : icon_1} />
                 </View>
                 <View className='input-view'>
                     <View className='input'>
-                        <Input 
-                            className='card-input' 
-                            type='text' 
-                            placeholder='想要什么卡？'
-                        />
+                        <Input className='card-input' type='text' placeholder='想要什么卡？' />
                     </View>
                 </View>
-                <View className='button-view' onClick={ this.props.onClick }>
+                <View className='button-view' onClick={this.props.onClick}>
                     <Text>
                         查询
                     </Text>
