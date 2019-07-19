bcd0bf0be7db5325d860dc417e5664e47fda0a9d
ChenMou <1106417223@qq.com>
2019-07-19 15:23:33 +0800

    详细信息填写页面

--- src/pages/detail-info/detail-info.jsx
+++ src/pages/detail-info/detail-info.jsx
@@ -101,6 +101,21 @@ export default class DetailInfo extends Component {
                     }, () => {console.log(this.state.infoLists.companyCharacter)})
                 }
             },
+            companyName: {
+                inputName: '单位名称',
+                finished: false,
+                onCompanyNameBlur: (e) => {
+                    let temp = this.state.infoLists;
+                    if (e.currentTarget.value != '') {
+                        temp.companyName.finished = true;
+                    } else {
+                        temp.companyName.finished = false;
+                    }
+                    this.setState({
+                        infoLists: temp
+                    })
+                }
+            },
             level: {
                 radioName: '职务',
                 finished: false,
@@ -114,6 +129,36 @@ export default class DetailInfo extends Component {
                     this.setState({
                         infoLists: temp
                     }, () => {console.log(this.state.infoLists.level)})
+                }
+            },
+            contactsName: {
+                inputName: '联系人姓名',
+                finished: false,
+                onContactsNameBlur: (e) => {
+                    let temp = this.state.infoLists;
+                    if (e.currentTarget.value != '') {
+                        temp.contactsName.finished = true;
+                    } else {
+                        temp.contactsName.finished = false;
+                    }
+                    this.setState({
+                        infoLists: temp
+                    })
+                }
+            },
+            contactsPhone: {
+                inputName: '联系人手机号',
+                finished: false,
+                onContactsPhoneBlur: (e) => {
+                    let temp = this.state.infoLists;
+                    if (e.currentTarget.value != '') {
+                        temp.contactsPhone.finished = true;
+                    } else {
+                        temp.contactsPhone.finished = false;
+                    }
+                    this.setState({
+                        infoLists: temp
+                    })
                 }
             },
             relationship: {
--- src/pages/detail-info/info-input/index.jsx
+++ src/pages/detail-info/info-input/index.jsx
@@ -1,8 +1,9 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View } from '@tarojs/components'
+import { View, Text } from '@tarojs/components'
 import './index.scss'
 
 import MyRadio from '@components/my-radio'
+import MyInput from '@components/my-input'
 
 export default class InfoInput extends Component {
     render () {
@@ -10,6 +11,16 @@ export default class InfoInput extends Component {
 
         return (
             <View>
+                <View className='info-input-pinyin'>
+                    <View className='info-input-pinyin-content'>
+                        <Text>姓名拼音</Text>
+                        <Text>CHEN XU TAO</Text>
+                    </View>
+                    <View className='info-input-pinyin-warn'>
+                        <Text>请核对，可修改，每字间请以单个空格隔开</Text>
+                    </View>
+                </View>
+
                 <MyRadio radioInfo={infoLists.idCard} onRadioClick={() => infoLists.idCard.onIdCardClick} />
                 <MyRadio radioInfo={infoLists.mariage} onRadioClick={() => infoLists.mariage.onMariageClick} />
                 <MyRadio radioInfo={infoLists.education} onRadioClick={() => infoLists.education.onEducationClick} />
@@ -17,6 +28,30 @@ export default class InfoInput extends Component {
                 <MyRadio radioInfo={infoLists.job} onRadioClick={() => infoLists.job.onJobClick} />
                 <MyRadio radioInfo={infoLists.companyCharacter} onRadioClick={() => infoLists.companyCharacter.onCompanyCharacterClick} />
                 <MyRadio radioInfo={infoLists.level} onRadioClick={() => infoLists.level.onLevelClick} />
+                <View className='info-input-item'>
+                    <MyInput 
+                      inputName={infoLists.companyName.inputName} 
+                      type='text' 
+                      finished={infoLists.companyName.finished} 
+                      onBlur={() => infoLists.companyName.onCompanyNameBlur}
+                    />
+                </View>
+                <View className='info-input-item'>
+                    <MyInput 
+                      inputName={infoLists.contactsName.inputName} 
+                      type='text' 
+                      finished={infoLists.contactsName.finished} 
+                      onBlur={() => infoLists.contactsName.onContactsNameBlur}
+                    />
+                </View>
+                <View className='info-input-item'>
+                    <MyInput 
+                      inputName={infoLists.contactsPhone.inputName} 
+                      type='number' 
+                      finished={infoLists.contactsPhone.finished} 
+                      onBlur={() => infoLists.contactsPhone.onContactsPhoneBlur}
+                    />
+                </View>
                 <MyRadio radioInfo={infoLists.relationship} onRadioClick={() => infoLists.relationship.onRelationshipClick} />
                 <MyRadio radioInfo={infoLists.postalAddress} onRadioClick={() => infoLists.postalAddress.onPostalAddressClick} />
             </View>
