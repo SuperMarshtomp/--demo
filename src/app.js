ed0c23d693f42dcd60eb36863759681443275fc6
ChenMou <1106417223@qq.com>
2019-07-19 12:01:02 +0800

    页面路径

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ class App extends Component {
   config = {
     pages: [
       // 'pages/index/index'
+      'pages/detail-info/detail-info',
       'pages/home/home',
       'pages/base-info/base-info',
     ],
